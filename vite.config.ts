2f49491c3193820f551e6a2ba7d53a2be81c5ca5
Phil Hennel <hennel@uni-bremen.de>
2025-07-16 13:37:37 +0200

    WiP

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import tailwindcss from "@tailwindcss/vite";
-//import { svelteTesting } from "@testing-library/svelte/vite";
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
-//import autoprefixer from 'autoprefixer'
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -24,14 +22,8 @@ export default defineConfig({
     cors: true
   },
   build: {
-    target: "modules",
-    sourcemap: true,
-    rollupOptions: {
-      external: [
-        "src/crawler/**/*",
-        "src/subvisor/**/*"
-      ],
-    },
+    target: "es2020",
+    sourcemap: true
   },
   preview: {
     host: true,
@@ -52,30 +44,4 @@ export default defineConfig({
       stream: "rollup-plugin-node-polyfills/polyfills/empty"
     }
   },
-  
-  // test: {
-  //   workspace: [
-  //     {
-  //       extends: "./vite.config.ts",
-  //       plugins: [svelteTesting()],
-  //       test: {
-  //         name: "client",
-  //         environment: "jsdom",
-  //         clearMocks: true,
-  //         include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-  //         exclude: ["src/lib/server/**"],
-  //         setupFiles: ["./vitest-setup-client.ts"]
-  //       }
-  //     },
-  //     {
-  //       extends: "./vite.config.ts",
-  //       test: {
-  //         name: "server",
-  //         environment: "node",
-  //         include: ["src/**/*.{test,spec}.{js,ts}"],
-  //         exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"]
-  //       }
-  //     }
-  //   ]
-  // }
 });
